82ed3d6f9869c6969d4cb462e5a8c03e711db564
XiaonanLu <luxiaonan.hubei@gmail.com>
2017-09-26 16:51:20 -0700

    Boston Housing project

---
+++ boston_housing/test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 24 22:41:37 2017
+
+@author: xiaonan
+"""
+
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+
+# Remove this deprecated module
+#from sklearn.cross_validation import ShuffleSplit
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+#%matplotlib inline
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+    
+# Success
+print ("Boston housing dataset has {} data points with {} variables each.".format(*data.shape))
